f364d12f4f36bbaa26ad214e0ab686c4ad3313a5
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-11 23:04:59 +0300

    Implement the reducer

--- src/redux/costants/index.js
+++ src/redux/costants/index.js
@@ -1,7 +1,6 @@
 const actionTypes = {
   SET_RECIPE: 'SET_RECIPE',
   SELECTED_RECIPE: 'SELECTED_RECIPE',
-  SEARCH_RECIPE: 'SEARCH_RECIPE',
   CHANGE_FILTER: 'CHANGE_FILTER',
   bookCategories: ['All', 'Beef', 'Chicken', 'Dessert', 'Lamb', 'Seafood', 'Breakfast', 'Vegetarian', 'Starter'],
 };
