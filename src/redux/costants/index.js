7144970ea2d0a85b673efd40301c371d6a0b619b
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-11 22:14:26 +0300

    Implement category

--- src/redux/costants/index.js
+++ src/redux/costants/index.js
@@ -2,7 +2,7 @@ const actionTypes = {
   SET_PRODUCT: 'SET_PRODUCT',
   SELECTED_PRODUCT: 'SELECTED_PRODUCT',
   REMOVE_SELECTED_PRODUCT: 'REMOVE_SELECTED_PRODUCT',
-  bookCategories: ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'],
+  bookCategories: ['All', 'Beef', 'Chicken', 'Dessert', 'Lamb', 'Seafood', 'Breakfast', 'Vegetarian', 'Starter'],
 };
 
 export default actionTypes;
