701f255c1bf20a02ed77ed89e8b712b012f89ba8
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-11 23:34:43 +0300

    Dispatch the api

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import axios from 'axios';
 import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setRecipe } from './../redux/actions/index';
 
 const Home = () => {
+  const food = useSelector(state => state.food);
+  const dispatch = useDispatch()
   const fetchProduct = async () => {
     const response = await axios.get('https://themealdb.com/api/json/v1/1/categories.php')
       .catch((error) => console.log(error));
-    console.log(response.data);
+      dispatch(setRecipe(response.data));
   };
   // const handleClick = () => {
   //   fetchProduct();
   // };
   useEffect(() => {
     // effect
-    return () => {
-      cleanup
-    }
+    // return () => {
+    //   cleanup
+    // }
+    dispatch(setRecipe)
+
   }, [input])
   return (
     <div>
--- src/redux/costants/index.js
+++ src/redux/costants/index.js
@@ -1,8 +1,7 @@
-const actionTypes = {
+export const actionTypes = {
   SET_RECIPE: 'SET_RECIPE',
   SELECTED_RECIPE: 'SELECTED_RECIPE',
   CHANGE_FILTER: 'CHANGE_FILTER',
   bookCategories: ['All', 'Beef', 'Chicken', 'Dessert', 'Lamb', 'Seafood', 'Breakfast', 'Vegetarian', 'Starter'],
 };
 
-export default actionTypes;
