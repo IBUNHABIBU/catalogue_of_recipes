e2abcbf84ce3f449c15985fde13230a73d754a04
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-12 00:45:31 +0300

    Fix the errors

--- src/containers/FoodListing.js
+++ src/containers/FoodListing.js
@@ -1,18 +1,22 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 const FoodListing = () => {
- const categories = useSelector(state => state.categories);
- const {idCategory, strCategory, strCategoryThumb, strCategoryDescription } = categories;
+  const categories = useSelector((state) => state);
+  console.log(categories);
+  // const {
+  //   idCategory, strCategory, strCategoryThumb, strCategoryDescription,
+  // } = categories;
 
- return (
-  <div>
-    <h1>{id}</h1>
-    <p>{strCategory}</p>
-    <img src={strCategoryThumb} alt="image" />
-    <p>{strCategoryDescription}</p>
-  </div>
- )
-}
+  return (
+    <div>
+     <h1>FoodListing</h1>
+      {/* <h1>{idCategory}</h1>
+      <p>{strCategory}</p>
+      <img src={strCategoryThumb} alt={strCategoryThumb} />
+      <p>{strCategoryDescription}</p> */}
+    </div>
+  );
+};
 
-export default FoodListing
+export default FoodListing;
