195f18897d50c1a49d857ca9f82468e3135bad76
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-11 23:23:40 +0300

    Fetch the api

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,13 +3,19 @@ import axios from 'axios';
 
 const Home = () => {
   const fetchProduct = async () => {
-    const response = await axios.get('https://themealdb.com/api/json/v1/1/filter.php?c=Seafood')
+    const response = await axios.get('https://themealdb.com/api/json/v1/1/categories.php')
       .catch((error) => console.log(error));
     console.log(response.data);
   };
-  const handleClick = () => {
-    fetchProduct();
-  };
+  // const handleClick = () => {
+  //   fetchProduct();
+  // };
+  useEffect(() => {
+    effect
+    return () => {
+      cleanup
+    }
+  }, [input])
   return (
     <div>
       <button type="submit" onClick={handleClick}>click Me</button>
