4dddcbfad662596bcc648cf011bdbf74280c75ed
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-12 16:01:50 +0300

    Add list to grid

--- src/containers/FoodListing.js
+++ src/containers/FoodListing.js
@@ -1,45 +1,28 @@
 import { Grid, Paper } from '@material-ui/core';
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Card } from '@material-ui/core/Card';
+import { CardHeader } from '@material-ui/core/CardHeader';
 
 const FoodListing = () => {
   const meals = useSelector((state) => state.allFoods.categories);
-  //   const renderList =
-  // <Grid container spacing={1} key={idCategory}>
-  //  {meals.map((meal) => {
-  //     const {
-  //       idCategory, strCategory, strCategoryThumb, strCategoryDescription,
-  //     } = meal;
-  //   }
-
-  //     return (
-
-  //         <Grid container item md={3} spacing={3}>
-  //           <Paper>
-  //             <p>{strCategory}</p>
-  //             <img src={strCategoryThumb} alt={strCategoryThumb} />
-  //             <p>{strCategoryDescription}</p>
-  //           </Paper>
-  //         </Grid>
-  //     );
-  //   });
-  //   </Grid>
   const renderList = (
     <Grid container>
       {
         meals.map((meal) => {
           const {
-                  idCategory, strCategory, strCategoryThumb, strCategoryDescription,
-                } = meal;
+            idCategory, strCategory, strCategoryThumb, strCategoryDescription,
+          } = meal;
           return (
             <Grid item lg={3} key={idCategory}>
               <Paper>
+                CardHeader
                 <p>{strCategory}</p>
                 <img src={strCategoryThumb} alt={strCategoryThumb} />
                 <p>{strCategoryDescription}</p>
               </Paper>
             </Grid>
-          )
+          );
         })
       }
     </Grid>
