56a6dc8dffe18c5d36a2b45b3153b6005ef377ca
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-12 15:48:04 +0300

    Add list to grid

--- src/containers/FoodListing.js
+++ src/containers/FoodListing.js
@@ -1,26 +1,32 @@
-import { Grid } from '@material-ui/core';
+import { Grid, Paper } from '@material-ui/core';
 import React from 'react';
 import { useSelector } from 'react-redux';
 
 const FoodListing = () => {
   const meals = useSelector((state) => state.allFoods.categories);
-  const renderList = meals.map((meal) => {
-    const {
-      idCategory, strCategory, strCategoryThumb, strCategoryDescription,
-    } = meal;
-    return (
-      <Grid container spacing={1} key={idCategory}>
-        <Grid container item xs={12} spacing={3}>
-          <Paper>
-            <p>{strCategory}</p>
-            <img src={strCategoryThumb} alt={strCategoryThumb} />
-            <p>{strCategoryDescription}</p>
-          </Paper>
-        </Grid>
-      </Grid>
-    );
-  });
+  //   const renderList =
+  // <Grid container spacing={1} key={idCategory}>
+  //  {meals.map((meal) => {
+  //     const {
+  //       idCategory, strCategory, strCategoryThumb, strCategoryDescription,
+  //     } = meal;
+  //   }
 
+  //     return (
+
+  //         <Grid container item md={3} spacing={3}>
+  //           <Paper>
+  //             <p>{strCategory}</p>
+  //             <img src={strCategoryThumb} alt={strCategoryThumb} />
+  //             <p>{strCategoryDescription}</p>
+  //           </Paper>
+  //         </Grid>
+  //     );
+  //   });
+  //   </Grid>
+  const renderList = (
+    <Grid container />
+  );
   return (
     <div>
       { renderList }
