5b91fdf00e0518da879faac0c0fa8ede22c5a3b8
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-13 11:07:26 +0300

    List all meals by categories

--- src/containers/CategoryListing.js
+++ src/containers/CategoryListing.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
+import { selectedRecipe } from '../redux/actions';
 
 const CategoryListing = () => {
   const { categoryId } = useParams();
   const categories = useSelector((state) => state.allFoods.categories);
   const filteredCat = categories.find((item) => item.idCategory === categoryId);
-  console.log(categories, `its id is ${categoryId} cat is ${filteredCat.strCategory}`);
+  const category = filteredCat.strCategory;
+  const dispatch = useDispatch()
 
   const fetchByCategory = async () => {
-    const response = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${}`)
+    const response = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`)
+    .catch(err=> console.log(error));
+    dispatch(selectedRecipe(response.data));
   }
   return (
     <div />
