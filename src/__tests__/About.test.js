fc5da8ec046bd739565267b892ed740757749c1c
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-16 07:47:05 +0300

    Test the about Page

--- src/__tests__/About.test.js
+++ src/__tests__/About.test.js
@@ -4,21 +4,19 @@ import About from '../containers/About';
 import { Provider } from 'react-redux';
 import { render } from '@testing-library/react';
 
-const setup = (props={}) => {
-  const comp = <Provider>
-    <About {...props} />
-  </Provider>
-  const component = shallow(comp);
-  return component;
-}
+// const setup = (props={}) => {
+//   const { getByText } = render(<About />);
+//   return component;
+// }
 
 describe('Testing the About page', () => {
-  let component;
-  beforeEach(() => {
-    component = setup();
-  })
+  // let component;
+  // beforeEach(() => {
+  //   component = setup();
+  // })
   it('should display the about component with className container', () => {
-    const wrapper = component.find('.container');
-    console.log(component.debug());
+    const { getByText } = render(<About />);
+    const wrapper = getByText('Learn different food recipes via this website');
+    expect(wrapper).toBeInTheDocument();
   });
 });
