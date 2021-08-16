0b05f037955b9c1d6d767d1f4a762a9fbdfb068a
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-16 06:47:47 +0300

    Test the about Page

--- src/__tests__/About.test.js
+++ src/__tests__/About.test.js
@@ -3,9 +3,17 @@ import { shallow } from 'enzyme';
 import About from '../containers/About';
 import { Provider } from 'react-redux'
 
+const setup = (props={}) => {
+  const component = shallow(<About {...props} />);
+  return component;
+}
+
 describe('Testing the About page', () => {
+  let component;
+  beforeEach(() => {
+    component = setup();
+  })
   it('should display the about component with className container', () => {
-    const component = shallow(<About />);
     const wrapper = component.find('.container');
     expect(wrapper.length).toBe(1);
   });
