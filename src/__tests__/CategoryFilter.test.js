7421b3dc071ad0d5a8c0d4b9ee7d0364c8a41781
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-17 19:07:09 +0300

    Test the props

--- src/__tests__/CategoryFilter.test.js
+++ src/__tests__/CategoryFilter.test.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import checkProps from './utility/index';
+import checkProps from '../utility/index';
+
 import CategoryFilter from './../components/CategoryFilter';
 
 describe('Checking Proptypes', () => {
   it('It should not throw a warning', () => {
     const expectedProp = {
-     handleFilter
+     handleFilter: () => {
+
+     }
     }
+
+    const propsError = checkProps(CategoryFilter, expectedProp);
+    expect(propsError).toBeUndefined();
   })
 })
--- src/__tests__/utility/index.js
+++
@@ -1,9 +0,0 @@
-import checkPropTypes from 'check-prop-types';
-
-const checkProps = (component, expectedProps) => {
-  const propError =
-  checkPropTypes(component.propTypes, expectedProps, 'props', component.name);
-  return propsError;
-}
-
-export default checkProps;
