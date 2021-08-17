a8e5f18697c90b6ec4836b47eb4dc9cf887b8eb7
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-17 16:41:33 +0300

    Refactor code

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,14 +1,14 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 import { useDispatch } from 'react-redux';
-import { setRecipe } from '../redux/actions/index';
+import { setCategories } from '../redux/actions/index';
 import FoodListing from './FoodListing';
 
 const Home = () => {
   const dispatch = useDispatch();
   const fetchProduct = async () => {
     const response = await axios.get('https://themealdb.com/api/json/v1/1/categories.php');
-    dispatch(setRecipe(response.data));
+    dispatch(setCategories(response.data));
   };
 
   useEffect(() => {
