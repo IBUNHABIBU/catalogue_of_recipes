6bb3dbed8ec5476653fc668fdc85ffafec218a4b
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-17 18:43:45 +0300

    Add checkPropTypes

--- src/__tests__/CategoryFilter.js
+++ src/__tests__/CategoryFilter.js
@@ -1,2 +1,10 @@
-import { checkPropTypes } from 'prop-types';
 import React from 'react';
+import { checkPropTypes } from 'check-prop-types';
+
+describe('Checking Proptypes', () => {
+  it('It should not throw a warning', () => {
+    const expectedProp = {
+     handleFilter
+    }
+  })
+})
