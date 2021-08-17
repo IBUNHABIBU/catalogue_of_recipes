ebe56a9dd2a1d4e16b385772c51107dced2a77d9
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-17 16:05:31 +0300

    Add reducer tests

--- src/__tests__/reducer.tes.js
+++ src/__tests__/reducer.tes.js
@@ -1,5 +1,6 @@
 import allMealCategoryReducer from './../redux/reducers/allMealCategoryReducer';
 import { actionTypes } from '../redux/costants';
+import { selectRecipe } from '../redux/actions';
 
 describe('allMealCategoryReducer', () => {
   it('Should return default state when there is no action', () => {
@@ -9,4 +10,18 @@ describe('allMealCategoryReducer', () => {
    };
     expect(newState).toEqual(defaultState);
   })
+
+  it('should return new state when give action', () => {
+    const data = {
+     categories: [{
+      id: 2,
+      name: 'Juma',
+      description: 'Lorem ipsum'm
+     }]
+    }
+    const newState = allMealCategoryReducer(undefined, {
+     type: actionTypes.SELECTED_CATEGORY,
+    payload: data })
+  });
+   expect(newState).toBe(data);
 })
