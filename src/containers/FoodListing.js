70e3c1c2b5be0dc51f4c4a5046ede26f622bedb3
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-19 21:52:44 +0300

    Style the Foodlisting

--- src/containers/FoodListing.js
+++ src/containers/FoodListing.js
@@ -2,10 +2,10 @@ import { Grid, Paper } from '@material-ui/core';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
+// import Card from '@material-ui/core/Card';
+// import CardHeader from '@material-ui/core/CardHeader';
+// import CardMedia from '@material-ui/core/CardMedia';
+// import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import useStyles from '../stylesheets/styles';
@@ -33,7 +33,7 @@ const FoodListing = () => {
           <Grid item lg={3} key={idCategory}>
             <Link to={`/category/${strCategory}`} className={classes.link}>
               <Paper spacing={3}>
-                <Card className={classes.root}>
+                {/* <Card className={classes.root}>
                   <CardHeader
                     title={strCategory}
                   />
@@ -47,7 +47,14 @@ const FoodListing = () => {
                       {strCategoryDescription.substring(0, 200)}
                     </Typography>
                   </CardContent>
-                </Card>
+                </Card> */}
+                <div className="card bg-dark text-white">
+  <img src={strCategoryThumb} className="card-img" alt="Thumb"/>
+  <div className="card-img-overlay">
+    <h5 className="card-title">{strCategory}</h5>
+    <p className="card-text"> {strCategoryDescription.substring(0, 200)}</p>
+  </div>
+</div>
               </Paper>
             </Link>
           </Grid>
