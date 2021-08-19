594d191a3bd756984d32f8e387e6d24cdeed667d
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-19 22:14:55 +0300

    Style the Foodlisting

--- src/containers/FoodListing.js
+++ src/containers/FoodListing.js
@@ -15,30 +15,30 @@ const FoodListing = () => {
     );
   }
   const renderList = (
-    <div className="row" spacing={1}>
-      {
-
-      meals.map((meal) => {
-        const {
-          idCategory, strCategory, strCategoryThumb, strCategoryDescription,
-        } = meal;
-        return (
-          <div className="col-3" key={idCategory}>
-            <Link to={`/category/${strCategory}`} className="link">
-              <div className="card">
-                <img src={strCategoryThumb} className="card-img-top" alt="Thumb" />
-                <div className="card-body">
-                  <h5 className="card-title">{strCategory}</h5>
-                  <p className="card-text">{strCategoryDescription.substring(0, 150)}</p>
-                  {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
+    <div className="p-4">
+      <div className="row" spacing={1}>
+        {
+        meals.map((meal) => {
+          const {
+            idCategory, strCategory, strCategoryThumb, strCategoryDescription,
+          } = meal;
+          return (
+            <div className="col-3" key={idCategory}>
+              <Link to={`/category/${strCategory}`} className="link">
+                <div className="card">
+                  <img src={strCategoryThumb} className="card-img-top" alt="Thumb" />
+                  <div className="card-body">
+                    <h5 className="card-title">{strCategory}</h5>
+                    <p className="card-text">{strCategoryDescription.substring(0, 150)}</p>
+                    {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
+                  </div>
                 </div>
-              </div>
-
-            </Link>
-          </div>
-        );
-      })
-      }
+              </Link>
+            </div>
+          );
+        })
+        }
+      </div>
     </div>
   );
   return (
