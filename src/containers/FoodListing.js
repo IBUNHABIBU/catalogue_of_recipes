5aa2892472bdec228d7c7f5643ee66180fbf8daa
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-19 21:57:59 +0300

    Style the Foodlisting

--- src/containers/FoodListing.js
+++ src/containers/FoodListing.js
@@ -2,10 +2,6 @@ import { Grid, Paper } from '@material-ui/core';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-// import Card from '@material-ui/core/Card';
-// import CardHeader from '@material-ui/core/CardHeader';
-// import CardMedia from '@material-ui/core/CardMedia';
-// import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import useStyles from '../stylesheets/styles';
@@ -33,26 +29,13 @@ const FoodListing = () => {
           <Grid item lg={3} key={idCategory}>
             <Link to={`/category/${strCategory}`} className={classes.link}>
               <Paper spacing={3}>
-                {/* <Card className={classes.root}>
-                  <CardHeader
-                    title={strCategory}
-                  />
-                  <CardMedia
-                    className={classes.media}
-                    image={strCategoryThumb}
-                    title="Paella dish"
-                  />
-                  <CardContent>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                      {strCategoryDescription.substring(0, 200)}
-                    </Typography>
-                  </CardContent>
-                </Card> */}
-                <div className="card bg-dark text-white">
-  <img src={strCategoryThumb} className="card-img" alt="Thumb"/>
-  <div className="card-img-overlay">
+               
+              <div className="card">
+  <img src={strCategoryThumb} className="card-img-top" alt="thumb" />
+  <div className="card-body">
     <h5 className="card-title">{strCategory}</h5>
-    <p className="card-text"> {strCategoryDescription.substring(0, 200)}</p>
+    <p className="card-text">{strCategoryDescription}</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
   </div>
 </div>
               </Paper>
