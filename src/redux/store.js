985edc3cc0a86c34a35c41fec7f956ea3737fc72
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-21 12:11:51 +0300

    Code refactor

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,9 @@
-/* eslint-disable no-underscore-dangle */
 import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import reducers from './reducers/index';
 
 export const middlewares = [thunk];
-
-// export const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore)
 
 export const myStore = composeWithDevTools(applyMiddleware(...middlewares))(createStore);
 
