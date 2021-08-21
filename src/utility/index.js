2792115f0977fffd0a090257bdb7b36ec3d0bcc7
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-21 12:14:03 +0300

    Code refactor

--- src/utility/index.js
+++ src/utility/index.js
@@ -1,17 +1,14 @@
 import checkPropTypes from 'check-prop-types';
 import { createStore, applyMiddleware } from 'redux';
-import { thunk } from 'redux-thunk';
 import reducers from '../redux/reducers';
+import { middlewares } from './../redux/store';
 
 export const checkProps = (component, expectedProps) => {
   const propsError = checkPropTypes(component.propTypes, expectedProps, 'props', component.name);
   return propsError;
 };
 
-export const findByTestAtrr = (initialState) => {
-
-};
 export const testStore = (initialState) => {
-  const createStoreWithMiddleWare = applyMiddleware(...thunk)(createStore);
+  const createStoreWithMiddleWare = applyMiddleware(...middlewares)(createStore);
   return createStoreWithMiddleWare(reducers, initialState);
 };
