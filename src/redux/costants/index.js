fa841ff1806132cd57a22a8c3922c1966d2fdedf
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-22 17:45:35 +0300

    Refactor code, renaming the files, methods and constants

--- src/redux/costants/index.js
+++ src/redux/costants/index.js
@@ -1,8 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 export const actionTypes = {
   SET_CATEGORIES: 'SET_CATEGORIES',
-  SELECT_CATEGORY: 'SELECT_CATEGORY',
   CHANGE_FILTER: 'CHANGE_FILTER',
-  SELECT_MEAL: 'SELECT_MEAL',
+  SELECT_ITEM: 'SELECT_ITEM',
   bookCategories: ['Beef', 'Chicken', 'Dessert', 'Lamb', 'Seafood', 'Breakfast', 'Vegetarian', 'Starter'],
 };
