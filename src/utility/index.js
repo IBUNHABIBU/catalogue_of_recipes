8284e73307a7f341538c9d6fc297bda999dec4b9
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-26 16:31:01 +0300

    Remove unused var

--- src/utility/index.js
+++ src/utility/index.js
@@ -2,15 +2,16 @@ import { render } from '@testing-library/react';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from '../redux/reducers';
 import { middlewares } from '../redux/store';
+import { Provider } from 'react-redux';
 
-const testStore = (initialState) => {
-  const createStoreWithMiddleWare = applyMiddleware(...middlewares)(createStore);
-  return createStoreWithMiddleWare(reducers, initialState);
-};
+// const testStore = (initialState) => {
+//   const createStoreWithMiddleWare = applyMiddleware(...middlewares)(createStore);
+//   return createStoreWithMiddleWare(reducers, initialState);
+// };
 
 export const renderWithRedux = (component, { initialState, store= createStore( reducers, initialState) } = {}) => {
   return {
     ...render(<Provider store={store}>{component}</Provider>)
   }
 }
-export default testStore;
+// export default testStore;
